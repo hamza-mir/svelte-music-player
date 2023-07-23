9a691e90ae1502c38ede6ee4158083fdb0369f1c
Hamza Mir <hamza.mir66@gmail.com>
2023-07-24 04:56:57 +0500

    initial commit

---
+++ svelte-course/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let name = "Svelte";
+	let src = "https://picsum.com/200/200";
+	let string = "Some <strong>string</strong> with html.";
+</script>
+
+<img {src} alt={name} />
+
+<h1>
+	Hello {name.toUpperCase()}
+</h1>
+<p>{@html string}</p>
+
+<style>
+	h1 {
+		color: aquamarine;
+	}
+</style>
