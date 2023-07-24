7acc2b189b7c3298aa643837631806c420b60fc5
Hamza Mir <hamza.mir66@gmail.com>
2023-07-24 05:14:34 +0500

    lec 2-4

--- svelte-course/src/lib/Counter.svelte
+++ svelte-course/src/lib/Counter.svelte
@@ -1,9 +1,16 @@
 <script>
     let count = 0;
 
+    $: string = `You have clicked ${count} times`;
+
+    $: if (count > 5) {
+        console.log(`The count is ${count}`);
+    }
+
     const increment = () => {
         count += 1;
     };
 </script>
 
 <button on:click={increment}>Clicks {count}</button>
+<h3>{string}</h3>
