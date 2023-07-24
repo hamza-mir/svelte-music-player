0d358398948473ef8ac0e0dccbd2e04c2b47037d
Hamza Mir <hamza.mir66@gmail.com>
2023-07-24 05:10:25 +0500

    lec 2-3

--- svelte-course/src/App.svelte
+++ svelte-course/src/App.svelte
@@ -1,18 +1,8 @@
 <script>
-	let name = "Svelte";
-	let src = "https://picsum.com/200/200";
-	let string = "Some <strong>string</strong> with html.";
+	import Counter from "./lib/Counter.svelte";
 </script>
 
-<img {src} alt={name} />
-
-<h1>
-	Hello {name.toUpperCase()}
-</h1>
-<p>{@html string}</p>
+<Counter />
 
 <style>
-	h1 {
-		color: aquamarine;
-	}
 </style>
