70b1f649d10de73cfea30cd3545925482bff1ad2
Hamza Mir <hamza.mir66@gmail.com>
2023-07-24 13:42:41 +0500

    2-6

--- svelte-course/src/lib/Counter.svelte
+++ svelte-course/src/lib/Counter.svelte
@@ -1,18 +1,13 @@
 <script>
-    let count1 = 0;
-    let count2 = 0;
+    export let initialCount;
+    export let maxCount;
 
-    const setCount2 = (x) => {
-        count2 = x;
-    };
+    let count = initialCount;
 
-    $: setCount2(count1);
-    $: string = `Count2 is ${count2}`;
-
-    const increment1 = () => {
-        count1 += 1;
+    const increment = () => {
+        if (count === maxCount) return;
+        count += 1;
     };
 </script>
 
-<button on:click={increment1}>Clicks {count1}</button>
-<p>{string}</p>
+<button on:click={increment}>Clicks {count}</button>
